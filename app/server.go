674a9e83af8f6bb962af9675a258c3eda8ad2281
Jason Buberel <jason@buberel.org>
2015-05-07 14:04:23 -0700

    Adding redis support

--- app/server.go
+++ app/server.go
@@ -15,6 +15,8 @@ import (
 
     "github.com/jbuberel/prime_finder/generator/eratosthenes"
     "github.com/jbuberel/prime_finder/generator/sundaram"
+
+    "github.com/garyburd/redigo/redis"
 )
 
 type PrimeGeneratorResult struct {
@@ -22,6 +24,7 @@ type PrimeGeneratorResult struct {
 	Prime    int64   `json:"prime"`
 	Limit    int64   `json:"limit"`
 	Duration float64 `json:"compute_time_sec"`
+  RedisCached bool `json:"redis_cached"`
 }
 
 // Response to all URLs beginning with '/prime'
@@ -45,6 +48,21 @@ func primeHandler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Using limit value: [%v]\n", limit)
 	}
 
+  log.Printf("About to retrieve key [%T] with value %v\n", strconv.Itoa(int(limit)), strconv.Itoa(int(limit)))
+  redisReturnVal, err := redis.String(redisConn.Do("GET", strconv.Itoa(int(limit))))
+  if err != nil {
+    log.Printf("redis get error: %v\n", err)
+  } else {
+    log.Printf("redisReturnVal is [%v] of type [%T]\n", redisReturnVal, redisReturnVal)
+  }
+
+  var redisCached bool = false
+  if redisReturnVal != "" {
+    redisCached = true
+  }
+
+
+
 	results := make([]PrimeGeneratorResult, 0)
 	prime, duration := sundaram.GetPrime(limit)
 	results = append(results, PrimeGeneratorResult{
@@ -52,6 +70,7 @@ func primeHandler(w http.ResponseWriter, r *http.Request) {
 		Prime:    prime,
 		Limit:    limit,
 		Duration: duration,
+    RedisCached: redisCached,
 	}) 
 
 	prime, duration = eratosthenes.GetPrime(limit)
@@ -60,6 +79,7 @@ func primeHandler(w http.ResponseWriter, r *http.Request) {
 		Prime:    prime,
 		Limit:    limit,
 		Duration: duration,
+    RedisCached: redisCached,
 	})
 	json_output, err := json.Marshal(results)
 	if err != nil {
@@ -70,6 +90,15 @@ func primeHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		fmt.Fprintf(w, string(json_output))
 	}
+
+  re, err := redisConn.Do("SET", strconv.Itoa(int(results[0].Limit)), strconv.Itoa(int(results[0].Prime)))
+  if err != nil {
+    log.Println("redis set error: %v\n", err)
+  } else {
+    log.Printf("set redis k/v as [%v]/[%v]\n", strconv.Itoa(int(results[0].Limit)), strconv.Itoa(int(results[0].Prime)))
+    log.Printf("redis returned: %T with value of %v\n", re, re)
+  }
+
 
   if err := db.Ping(); err != nil {
     return
@@ -107,40 +136,40 @@ func resultsHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Add("Content-type", "text/plain")
   w.WriteHeader(200)
 
+
+
+
   if err := db.Ping(); err != nil {
     fmt.Fprintf(w,"No DB connection\n")
     return
   }
   rows, err := db.Query("SELECT limit_val, prime FROM primes")
   if err != nil {
-          log.Fatal(err)
+          log.Println(err)
   }
   defer rows.Close()
-  fmt.Fprintf(w, "Primes are:\n\n")
+  fmt.Fprintf(w, "MySQL Primes are:\n\n")
   for rows.Next() {
     var limit int
     var prime int
     if err := rows.Scan(&limit, &prime); err != nil {
-            log.Fatal(err)
+            log.Println(err)
     }
     fmt.Fprintf(w,"Limit: %v - Prime: %v\n", limit, prime)
   }
   if err := rows.Err(); err != nil {
-          log.Fatal(err)
+          log.Println(err)
   }
 
 
 }
 
 var db  *sql.DB
+var redisConn redis.Conn
+
 var mysql string
 
 func init() {
-  defer func() {
-        if r := recover(); r != nil {
-            fmt.Println("Recovered in f", r)
-        }
-    }()
   var err error
 
 
@@ -163,10 +192,19 @@ func init() {
     }
   }
 
+
+  redisConn, err = redis.Dial("tcp", ":6379")
+  if err != nil {
+    log.Println(err)
+  }
+  log.Printf("redisConn: %v\n", redisConn)
+
+
   sysLogger, err := syslog.New(syslog.LOG_NOTICE, "prime_finder")
   if err == nil {
     log.SetOutput(sysLogger)
   }
+
 
 }
 
@@ -176,6 +214,9 @@ func main() {
             fmt.Println("Recovered in f", r)
         }
     }()
+    defer redisConn.Close()
+    defer db.Close()
+
     http.HandleFunc("/prime", primeHandler)
     http.HandleFunc("/_ah/health", healthHandler)
     http.HandleFunc("/results", resultsHandler)
